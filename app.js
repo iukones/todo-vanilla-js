f269db9e5d321aca10bd430c763803ca7b83a772
Israel Martinez <iukones1@gmail.com>
2020-05-22 19:23:02 -0500

    finish global styles in project

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log('Estoy en app.js');
+
+// Selectors
+const $input = document.querySelector(".form__input");
+const $button = document.querySelector(".form__button");
+const $list = document.querySelector(".todo__list");
+
+
+// Event Listeners
+$button.addEventListener('click', addItem);
+
+
+
+
+// Function
+
+function addItem(event) {
+  event.preventDefault();
+  console.log(`Hola ${event}`);
+
+}
