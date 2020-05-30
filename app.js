68606321fd59ddd175b32acaa7f5d0c1117e3e49
Israel Martinez <iukones1@gmail.com>
2020-05-30 01:32:13 -0500

    add functions get and delete items in localStorage

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log('Estoy en app.js');
 
 // Selectors
 const $input = document.querySelector('.form__input');
@@ -8,6 +7,7 @@ const $filterData = document.querySelector('.todo__filter');
 
 
 // Event Listeners
+document.addEventListener('DOMContentLoaded', getLocalItem);
 $button.addEventListener('click', addItem); //event add new item with function addItem.
 $list.addEventListener('click', deleteCheckItem);
 $filterData.addEventListener('click', filterTodo);
@@ -29,9 +29,9 @@ function addItem(event) {
   $listItem.innerText = $input.value;
   $itemDiv.appendChild($listItem);
   // console.log($itemDiv);
+
   //Save localStorage
   saveLocalTodos($input.value);
-
 
   //create button 🚫 
   const $trashButton = document.createElement('button');
@@ -63,8 +63,9 @@ function deleteCheckItem(event){
     // console.log($item.classList[0]);
     const $todo = $item.parentElement;
     // console.log($item.parentElement);
-    const $request = confirm(`Deseas eliminar el artículo...`);
+    const $request = confirm(`Deseas eliminar "${$todo.children[0].innerText}"`);
     if($request === true) {
+      removeLocalItem($todo);
       $todo.remove();
     }
     //animation
@@ -108,28 +109,92 @@ function filterTodo(event) {
           break;
       }
     }
-    return;
   });
 }
 
+
 function saveLocalTodos(todo) {
   let $items;
+  
+  if(localStorage.getItem('$items') === null) {
+    $items = [];
+  }
+  else {
+    $items = JSON.parse(localStorage.getItem('$items'));
+    console.log($items);
+  }
+  
+  // Array de objetos
+  /* let data = {
+      name: todo,
+      status: "uncompleted"
+  }; */
 
+  $items.push(todo);
+  // alert($items);
+
+  localStorage.setItem('$items', JSON.stringify($items));
+
+}
+
+function getLocalItem() {
+  let $items;
+  
+  if(localStorage.getItem('$items') === null) {
+    $items = [];
+  }
+  else {
+    $items = JSON.parse(localStorage.getItem('$items'));
+    // console.log($items);
+  }
+
+  $items.forEach(function(todo) {
+      // todo div
+    // console.log(`Hola ${event}`);
+    const $itemDiv = document.createElement('div');
+    $itemDiv.classList.add('todo__div');
+    // console.log($itemDiv);
+
+    // create list 🔥 
+    const $listItem = document.createElement('li');
+    $listItem.classList.add('todo__item');
+    $listItem.innerText = todo;
+    $itemDiv.appendChild($listItem);
+    // console.log($itemDiv);
+
+    //create button 🚫 
+    const $trashButton = document.createElement('button');
+    $trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    $trashButton.classList.add('trash__btn');
+    $itemDiv.appendChild($trashButton);
+
+    //create button ✔ 
+    const $completeButton = document.createElement('button');
+    $completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    $completeButton.classList.add('complete__btn');
+    $itemDiv.appendChild($completeButton);
+    
+    //add to list item
+    $list.appendChild($itemDiv);
+
+  });
+}
+
+function removeLocalItem(todo) {
+  
+  let $items;
+  
   if(localStorage.getItem('$items') === null) {
     $items = [];
   }
   else {
     $items = JSON.parse(localStorage.getItem('$items'));
   }
-
-  let data = {
-      name: todo,
-      status: "uncompleted"
-  };
-
-  $items.push(data);
-  alert(data);
-
+  
+  const todoIndex = todo.children[0].innerText;
+  $items.splice($items.indexOf(todoIndex), 1);
   localStorage.setItem('$items', JSON.stringify($items));
-
+  // console.log(todo.children[0].innerText);
+  // console.log($items.indexOf('hola 1'));
+  
 }
